1bdadd5005d1e494be440211dda8df0f612fcc26
facquer <angel.ichazu@gmail.com>
2020-02-04 11:50:42 -0500

    Validaciones de registrar paciente

--- src/main/java/Negocio/GestionPaciente.java
+++ src/main/java/Negocio/GestionPaciente.java
@@ -7,6 +7,8 @@ import javax.inject.Inject;
 
 import Datos.PacienteDAO;
 import Datos.RolDAO;
+import Exception.ExceptionDigitalMedical;
+import Exception.MetodosVarios;
 import Modelo.Paciente;
 import Modelo.Rol;
 
@@ -17,6 +19,37 @@ public class GestionPaciente implements GestionPacienteLocal {
 	private PacienteDAO pdao;
 	@Inject
 	private RolDAO rdao;
+	@Inject
+	private MetodosVarios mv;
+	
+	
+	public void insertar2(Paciente paciente) throws ExceptionDigitalMedical {
+		List<Paciente> p = pdao.buscar1Paciente(paciente.getCedula());
+		if (p.isEmpty()) {
+			if (paciente.getCedula().length() == 10) {
+				if (mv.numerico(paciente.getCedula()) == true) {
+					if (mv.soloLetras(paciente.getNombre()) == true & mv.soloLetras(paciente.getApellido()) == true) {
+						if (mv.soloLetras(paciente.getPreguntaSecreta()) == true) {
+							pdao.insertar(paciente);
+						} else {
+							throw new ExceptionDigitalMedical(6);
+						}
+
+					} else {
+						throw new ExceptionDigitalMedical(4);
+					}
+				} else {
+					throw new ExceptionDigitalMedical(3);
+				}
+
+			} else {
+				throw new ExceptionDigitalMedical(2);
+			}
+
+		} else {
+			throw new ExceptionDigitalMedical(1);
+		}
+	}
 
 	@Override
 	public void insertar(Paciente paciente) {
