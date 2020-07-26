bc95a61333460be3b0f6d68ec533b401a9f39f1d
bvega <vegabryam40@gmail.com>
2020-07-26 15:26:24 -0500

    creando servicio get

--- src/main/java/Servicios/RestServices.java
+++ src/main/java/Servicios/RestServices.java
@@ -1,15 +1,20 @@
 package Servicios;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 import Modelo.Medicina;
 import Modelo.Medico;
+import Modelo.Rol;
 import Negocio.GestionMedicinaLocal;
 import Negocio.GestionMedicoLocal;
+import Negocio.GestionRolLocal;
 
 @Path("/practicaws")
 public class RestServices {
@@ -19,6 +24,9 @@ public class RestServices {
 	
 	@Inject
 	private GestionMedicinaLocal gmel;
+	
+	@Inject
+	private GestionRolLocal grl;
 	
 	@POST
 	@Path("/crearMedico")
@@ -31,11 +39,18 @@ public class RestServices {
 	
 	@POST
 	@Path("/crearMedicamento")
-	@Consumes("application/json")
 	@Produces("application/json")
+	@Consumes("application/json")
 	public String insertarMedicamento(Medicina medicina) {
 		this.gmel.insertar(medicina);
 		return "OK";
+	}
+	
+	@GET
+	@Path("/listarRol")
+	@Produces("application/json")
+	public List<Rol> listarRol() {
+		return this.grl.getRoles();
 	}
 
 }
